bb9795a3d312f9f1599457c50664186bcafaed4c
melmatx <melpalana13@gmail.com>
2023-10-20 08:51:08 +0800

    Created placeholder for navigation screens

---
+++ navigation/MainNavigation.js
@@ -0,0 +1,11 @@
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+const MainNavigation = () => (
+    <Stack.Navigator>
+
+    </Stack.Navigator>
+);
+
+export default MainNavigation;
